720d013b57cf7ee689da28d618788e4f2d1e8459
Alex Edwards <Alex Edwards>
2017-06-07 13:54:43 -0700

    update

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
 //fill
+var showOrderList = false;
+
+document.querySelector('.site-title').addEventListener('click', function(event){
+	toggleOrderDisplay();
+});
+
+function toggleOrderDisplay (){
+	var orderList = document.querySelector('.orderList');
+	if(showOrderList) orderList.style.transform =  'translate(0, 100px)'; //move down
+	else orderList.style.transform = 'translate(0, 0)';
+	showOrderList = !showOrderList;
+}
